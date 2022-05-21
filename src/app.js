3c6299f7e56dac989398c431815c47a62e39dba8
bpmurray77 <bpmurray77@gmail.com>
2022-05-21 00:07:25 +0000

    Start

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let first = ["massive", "obtuse"];
+  let second = ["crystaline", "amethyst"];
+  let third = ["palace", "fortress"];
+  let list = [];
+  var total = first.length * second.length * third.length;
+  for (var i = 0; i < total; i++) {
+    for (var a = 0; a < first.length - 1; a++) {
+      list.push(first[a] + second[b] + third[c]);
+      for (var b = 0; b < second.length - 1; b++) {
+        list.push(first[a] + second[b] + third[c]);
+        for (var c = 0; c < third.length - 1; c++) {
+          list.push(first[a] + second[b] + third[c] + ".com");
+        }
+      }
+    }
+  }
+  console.log(list);
 };
