bdb248737cf432702ff1c85a218d234bd7397401
bpmurray77 <bpmurray77@gmail.com>
2022-06-03 22:45:59 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let first = ["massive", "obtuse"];
-  let second = ["crystaline", "amethyst"];
-  let third = ["palace", "fortress"];
+  let first = ["massive", "obtuse", "towering"];
+  let second = ["crystaline", "amethyst", "marble"];
+  let third = ["palace", "fortress", "spire"];
   let list = [];
   var total = first.length * second.length * third.length;
   //  for (var i = 0; i < total; i++) {
   for (var a = 0; a < first.length; a++) {
     for (var b = 0; b < second.length; b++) {
       for (var c = 0; c < third.length; c++) {
-        list.push(first[a] + second[b] + third[c] + ".com");
+        list.push("  " + first[a] + second[b] + third[c] + ".com");
       }
     }
   }
